fd90e933c38e95626eb3c3c6a1c8857cfdd37fea
Hector Flores <hecto932@gmail.com>
2019-02-21 16:09:54 -0600

    Ready to production

--- routes/views/products.js
+++ routes/views/products.js
@@ -4,7 +4,13 @@ const ProductsService = require('../../services/products')
 
 const productService = new ProductsService();
 
+const cacheResponse = require('../../utils/cacheResponse');
+const { FIVE_MINUTES_IN_SECONDS } = require('../../utils/time');
+
 router.get('/', async function (req, res, next) {
+  // ADDING CACHE FOR LIST 5 minutes
+  
+  cacheResponse(res, FIVE_MINUTES_IN_SECONDS)
   const { tags } = req.query;
   try { 
     // throw new Error('Its not working from view');
